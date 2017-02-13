dbf39d34986441e5a34eceff1f16a2d504d04178
litao <litao@guoshengtianfeng.com>
2017-02-13 16:57:11 +0800

    注释完善

--- src/main/java/com/lt/thread/concurrent/UseCountDownLatch.java
+++ src/main/java/com/lt/thread/concurrent/UseCountDownLatch.java
@@ -2,6 +2,11 @@ package com.lt.thread.concurrent;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * CountDownLatch : 一个线程(或者多个)， 等待另外N个线程完成某个事情之后才能执行。   
+ * CyclicBarrier        : N个线程相互等待，任何一个线程完成之前，所有的线程都必须等待。
+ *
+ */
 public class UseCountDownLatch {
 
 	public static void main(String[] args) {
